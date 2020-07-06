45130982ccc4693bfe10da767f2ad3483ae3a8cf
macT <simmonst208@gmail.com>
2020-07-05 20:51:54 -0600

    ReadMe is complete as well as basic JS functionality, wish I had more time to work on LocalStorage issues as well as a more effecient manner to code out this project as is very repetitive

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
-// create calendar display elements the current day && elements for each business hour of the day (8-5)
 
-// allow a user to inject their plan for that hour block and save it inside local storage
-
-// change text and color coordinate for the past present and future
 
 var day = document.querySelector(".container");
 var currentDay = document.querySelector("#currentDay");
 var today = moment();
 $("#currentDay").text("Today is " + today.format("MM-DD-YYYY"));
-
 var hour8 = document.querySelector(".eight");
 var hour9 = document.querySelector(".nine");
 var hour10 = document.querySelector(".ten");
@@ -19,6 +14,9 @@ var hour2 = document.querySelector(".two");
 var hour3 = document.querySelector(".three");
 var hour4 = document.querySelector(".four");
 var hour5 = document.querySelector(".five");
+
+
+
 
 
 if ( today.isAfter(moment("08:00", "hh:mm"))){
@@ -88,80 +86,65 @@ if ( today.isAfter(moment("17:00", "hh:mm"))){
 $(hour8).on("click", function(){
    var plan8 = prompt("What would you like to schedule?");
    $("#8").text(plan8);
-   localStorage.setItem("8am plans", plan8);
+   localStorage.setItem("8plans", plan8);
 });
 
 
 $(hour9).on("click", function(){
     var plan9 = prompt("What would you like to schedule?");
     $("#9").text(plan9);
-    localStorage.setItem("9am plans", plan9);
+    localStorage.setItem("9plans", plan9);
  });
 
  $(hour10).on("click", function(){
     var plan10 = prompt("What would you like to schedule?");
     $("#10").text(plan10);
-    localStorage.setItem("10am plans", plan10);
+    localStorage.setItem("10plans", plan10);
  });
  
  $(hour11).on("click", function(){
      var plan11 = prompt("What would you like to schedule?");
      $("#11").text(plan11);
-     localStorage.setItem("11am plans", plan11);
+     localStorage.setItem("11plans", plan11);
   });
 
   $(hour12).on("click", function(){
     var plan12 = prompt("What would you like to schedule?");
     $("#12").text(plan12);
-    localStorage.setItem("12pm plans", plan12);
+    localStorage.setItem("12plans", plan12);
  });
  
  $(hour1).on("click", function(){
      var plan1 = prompt("What would you like to schedule?");
      $("#1").text(plan1);
-     localStorage.setItem("1pm plans", plan1);
+     localStorage.setItem("1plans", plan1);
   });
 
   $(hour2).on("click", function(){
     var plan2 = prompt("What would you like to schedule?");
     $("#2").text(plan2);
-    localStorage.setItem("2pm plans", plan2);
+    localStorage.setItem("2plans", plan2);
  });
  
  $(hour3).on("click", function(){
      var plan3 = prompt("What would you like to schedule?");
      $("#3").text(plan3);
-     localStorage.setItem("3pm plans", plan3);
+     localStorage.setItem("3plans", plan3);
   });
 
   $(hour4).on("click", function(){
     var plan4 = prompt("What would you like to schedule?");
     $("#4").text(plan4);
-    localStorage.setItem("4pm plans", plan4);
+    localStorage.setItem("4plans", plan4);
  });
 
  
- $(hour5).on("click", function(){
+ $(hour5).on("click", function(){ //click event 
      var plan5 = prompt("What would you like to schedule?");
      $("#5").text(plan5);
-     localStorage.setItem("5pm plans", plan5);
-  });
+     localStorage.setItem("5plans", plan5);
+  }); 
 
- 
+  
 
- 
- 
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
+  
